a7412cd9eb1bb50bdd01a869ce342020722c0fc0
Victor Vilela Duarte <victorvduarte@gmail.com>
2024-05-29 01:15:19 -0300

    formating to BRL

--- scripts.js
+++ scripts.js
@@ -3,5 +3,19 @@ const amount = document.getElementById('amount');
 
 amount.oninput = () =>{
   let value = amount.value.replace(/\D/g , '');
-  amount.value = value;
+
+  //tranformar o valor em centavos
+  value = Number(value) / 100;
+
+
+  amount.value = formatCurrencyBRL(value);
+}
+
+function formatCurrencyBRL(value){
+  value = value.toLocaleString("pt-BR",{
+    style: "currency",
+    currency: "BRL"
+  });
+
+  return value;
 }
