9d0a3ce8dd1dafe4ded9bc939db553ee32627896
Victor Vilela Duarte <victorvduarte@gmail.com>
2024-05-28 23:24:06 -0300

    validating currency input

---
+++ scripts.js
@@ -0,0 +1,7 @@
+//seleção de elementos do form
+const amount = document.getElementById('amount');
+
+amount.oninput = () =>{
+  let value = amount.value.replace(/\D/g , '');
+  amount.value = value;
+}
