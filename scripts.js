12e6bfc8e87450efcaf462d4120a00ea8911c567
Victor Vilela Duarte <victorvduarte@gmail.com>
2024-05-29 01:35:21 -0300

    adding expense on list

--- scripts.js
+++ scripts.js
@@ -1,12 +1,15 @@
-//seleção de elementos do form
+//seleção de elementos do HTML
+const form = document.querySelector('form');
 const amount = document.getElementById('amount');
+const expense = document.getElementById('expense');
+const category = document.getElementById('category');
+const expenseList = document.querySelector('ul');
 
 amount.oninput = () =>{
   let value = amount.value.replace(/\D/g , '');
 
   //tranformar o valor em centavos
   value = Number(value) / 100;
-
 
   amount.value = formatCurrencyBRL(value);
 }
@@ -18,4 +21,49 @@ function formatCurrencyBRL(value){
   });
 
   return value;
+}
+
+form.onsubmit = (event) =>{
+  event.preventDefault();
+
+  const newExpense = {
+    id: new Date().getTime(),
+    expense: expense.value,
+    category_id: category.value,
+    category_name: category.options[category.selectedIndex].text,
+    amount: amount.value,
+    created_at: new Date(),
+  }
+
+  addExpense(newExpense);
+}
+
+function addExpense(newExpense){
+  try{
+    const expenseItem = document.createElement("li");
+    expenseItem.classList.add("expense");
+
+    const expenseIcon = document.createElement("img");
+    expenseIcon.setAttribute("src", `img/${newExpense.category_id}.svg`);
+    expenseIcon.setAttribute("alt", newExpense.category_name);
+
+    const expenseInfo = document.createElement("div");
+    expenseInfo.classList.add("expense-info");
+
+    const expenseName = document.createElement("strong");
+    expenseName.textContent = newExpense.expense;
+
+    const expenseCategory = document.createElement("span");
+    expenseCategory.textContent = newExpense.category_name;
+
+    expenseInfo.append(expenseName, expenseCategory);
+
+    expenseItem.append(expenseIcon, expenseInfo);
+
+    expenseList.append(expenseItem);
+
+  } catch(error) {
+    console.log(error);
+    alert("Erro ao adicionar despesa.");
+  }
 }
