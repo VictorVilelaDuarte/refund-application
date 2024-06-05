a54277a522e3711d94ba22c28247157a0d014c70
Victor Vilela Duarte <victorvduarte@gmail.com>
2024-06-05 00:21:39 -0300

    changing aside header content

--- scripts.js
+++ scripts.js
@@ -4,6 +4,8 @@ const amount = document.getElementById('amount');
 const expense = document.getElementById('expense');
 const category = document.getElementById('category');
 const expenseList = document.querySelector('ul');
+const expensesQuantity = document.querySelector("aside header p span");
+const expensesTotal = document.querySelector("aside header h2");
 
 amount.oninput = () =>{
   let value = amount.value.replace(/\D/g , '');
@@ -71,8 +73,42 @@ function addExpense(newExpense){
 
     expenseList.append(expenseItem);
 
+    updateTotals();
+
   } catch(error) {
     console.log(error);
     alert("Erro ao adicionar despesa.");
+  }
+}
+
+function updateTotals(){
+  try{
+    const items = expenseList.children
+
+    expensesQuantity.textContent = `${items.length} ${items.length > 1 ? 'despesas' : 'despesa'}`;
+
+    let total = 0;
+
+    console.log(total);
+    for (let item = 0; item < items.length; item++){
+      const itemAmount = items[item].querySelector(".expense-amount");
+      let value = itemAmount.textContent.replace(/[^\d,]/g, "").replace(",",".");
+      value = parseFloat(value);
+
+      if(isNaN(value)){
+        return alert("Erro ao converter os totais.")
+      }
+      total += Number(value); 
+    }
+
+    const symbolBRL = document.createElement("small");
+    symbolBRL.textContent = "R$";
+    total = formatCurrencyBRL(total).toUpperCase().replace("R$", "");
+
+    expensesTotal.innerHTML = ""
+    expensesTotal.append(symbolBRL, total);
+  } catch (error){
+    console.log(error);
+    alert("Não foi possível atualizar os totais.")
   }
 }
